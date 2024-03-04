9e65e0e6f9006dfc27a1aa3824e86e4a1294410b
OrlandoPMercado <162064095+OrlandoPMercado@users.noreply.github.com>
2024-03-03 20:03:06 -0600

    Add files via upload
    aqui se puede ver el programa que resuelve los problemas del cap 2al 9

---
+++ _Practica02_09.py
@@ -0,0 +1,19 @@
+
+nombre= "orlando" .title()# En esta linea y la siguiente se crean dos variables
+apellido= "perez" # se utilizan los metodos title para poner en mayuscula la primera letra a la hora de imprimir
+print( '"eSte ProGrama Fue cReado por:"\t' .lower() + nombre + "\t" + apellido .title() )# En esta linea se concatena valores string y variables string a la vez que se implementa el metodo lower.
+print("\n En este programa se da solucion a los ejercicios de los capitulos 2-9\n")# En esta linea como en la anterior se implementan brinco de espacio, de linea y el manejo de los dos tipos de comillas.
+suma = 35 + 15
+resta = 35 -23
+
+div = suma / resta
+mul = div * 3
+
+pot = mul ** 2
+
+resul = round(pot,1)  
+print ("el resultado de las operaciones ejecutadas es:\t ")
+print ( resul)
+
+#se puede observar que se utilizaron  operadores para generar operaciones basicas con valores in y float  mediante sus valores como por variables
+# tambien se ve el uso de la funcion round para redondear los decimales.
